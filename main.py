c3f45068745129e2d388a19f783d4bef271d4e94
raeperd <raeperd117@gmail.com>
2021-06-22 21:51:19 +0900

    Combine all code in single python script

--- main.py
+++ main.py
@@ -1,17 +1,45 @@
-import logging
+import argparse
 from pathlib import Path
+from typing import List
+from http.client import HTTPSConnection
+from base64 import b64encode
 
-from src.typora_argument_parser import TyporaArgumentParser
-from src.imgbb_uploader import ImgBBUploader
+import logging
+
+
+class TyporaArgumentParser:
+    def __init__(self):
+        self._parser = argparse.ArgumentParser()
+        self._parser.add_argument("files", type=Path, nargs='*')
+
+    def parse(self) -> List[Path]:
+        return vars(self._parser.parse_args())['files']
+
+
+class ImgBBUploader:
+    def __init__(self, api_key: str):
+        self._address = "https://api.imgbb.com/1/upload"
+        self._api_key = api_key
+
+    def upload_image(self, image_file: Path):
+        connection = HTTPSConnection("api.imgbb.com")
+        connection.request("POST", f"/1/upload?key={self._api_key}&name={image_file.stem}",
+                           f"image={b64encode(image_file.read_bytes()).decode()}",
+                           headers={"Content-Type": "application/x-www-form-urlencoded"})
+        return connection.getresponse()
+
+
+####################################################################################################
+API_KEY = "YOUR IMGBB API KEY HERE"
+
 
 logging.basicConfig()
 LOG = logging.getLogger()
 
 if __name__ == '__main__':
-    API_KEY = Path(__file__).parent.with_name("APIKEY").read_text()
-    if not API_KEY:
+    if not API_KEY or API_KEY == "YOUR IMGBB API KEY HERE":
         LOG.error(
-            f"Must set ImgBB API-KEY to environment variable $TYPORA_IMAGE_UPLOADER_API_KEY")
+            f"Must set ImgBB API-KEY to upload")
         exit()
 
     parser = TyporaArgumentParser()
